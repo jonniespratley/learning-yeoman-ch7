9c488e8a3053e799f37701d92c4a7d4e139825e0
Jonnie Spratley <jonniespratley@me.com>
2014-03-31 20:17:20 -0700

    added files

---
+++ jps-commonjs/dist/jps-rest.js
@@ -0,0 +1,12 @@
+/*! jps-rest - v0.0.0 - 2014-03-31
+* https://github.com/jonniespratley/jps-rest
+* Copyright (c) 2014 Jonnie Spratley; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
---
+++ jps-commonjs/src/jps-rest.js
@@ -0,0 +1,17 @@
+/*
+ * jps-rest
+ * https://github.com/jonniespratley/jps-rest
+ *
+ * Copyright (c) 2014 Jonnie Spratley
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
