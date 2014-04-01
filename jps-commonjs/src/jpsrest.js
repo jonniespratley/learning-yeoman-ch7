12e82effd9328c937707adf32c21d16af74a8829
Jonnie Spratley <jonniespratley@me.com>
2014-03-31 20:28:42 -0700

    added files

---
+++ jps-commonjs/src/jpsrest.js
@@ -0,0 +1,17 @@
+/*
+ * jps.rest
+ * https://github.com//jpsrest
+ *
+ * Copyright (c) 2014 
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
