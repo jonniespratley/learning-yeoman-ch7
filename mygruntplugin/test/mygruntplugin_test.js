ba563f7644871e37306f74c9d87bb26afc96444b
Jonnie Spratley <jonniespratley@gmail.com>
2014-08-14 05:02:11 +0000

    updated code

--- mygruntplugin/test/mygruntplugin_test.js
+++ mygruntplugin/test/mygruntplugin_test.js
@@ -1,26 +1,22 @@
 'use strict';
 var grunt = require('grunt');
 exports.learning_yeoman_ch8 = {
-  setUp: function (done) {
+  setUp: function(done) {
     // setup here if necessary
     done();
   },
-  default_options: function (test) {
+  default_options: function(test) {
     test.expect(1);
-
     var actual = grunt.file.read('tmp/default_options');
     var expected = grunt.file.read('test/expected/default_options');
     test.equal(actual, expected, 'should perform task with default options.');
-
     test.done();
   },
-  custom_options: function (test) {
+  custom_options: function(test) {
     test.expect(1);
-
     var actual = grunt.file.read('tmp/custom_options');
     var expected = grunt.file.read('test/expected/custom_options');
     test.equal(actual, expected, 'should perform task with custom options.');
-
     test.done();
   }
 };
